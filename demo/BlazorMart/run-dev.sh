aac8e1189303005c61249098da8e30ebae43e3a9
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-11-28 16:43:46 +0000

    Embed HTTPS cert into server image, since it's not used externally

--- demo/BlazorMart/run-dev.sh
+++ demo/BlazorMart/run-dev.sh
@@ -1,23 +1,9 @@
-# Before this will work, you need to create a dev cert at ~/.aspnet/https/aspnetapp.pfx
-# with password 'mycertpassword'
-# For example, run:
-#    dotnet dev-certs https -ep ${HOME}/.aspnet/https/aspnetapp.pfx -p mycertpassword
-# or run the equivalent for Windows then copy the .pfx file to ~/.aspnet/https/
-# More info at https://docs.microsoft.com/en-us/aspnet/core/security/docker-https?view=aspnetcore-3.0
-# Unfortunately it's not enough to run without HTTPS because gRPC only allows insecure connections
-# if you only enable HTTP2 protocol. We need HTTP1 as well for gRPC-Web.
-
 ./build-images.sh
 
 # Start client
 docker run --rm -d -p 8000:80 --name blazormart-client-instance blazormart-client
 
 # Start server
-docker create --rm -p 8001:443 \
-    -e ASPNETCORE_URLS="https://+;http://+" \
-    -e ASPNETCORE_Kestrel__Certificates__Default__Password="mycertpassword" \
-    -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx \
-    --name blazormart-server-instance blazormart-server
-docker cp ~/.aspnet/https blazormart-server-instance:/https/
+docker create --rm -p 8001:443 --name blazormart-server-instance blazormart-server
 docker start -i blazormart-server-instance
 docker kill blazormart-client-instance
