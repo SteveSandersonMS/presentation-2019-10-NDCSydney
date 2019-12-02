2d2ef0613ab6c7711968dd015599bd23e5221d1c
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-12-02 13:34:02 +0000

    Improve scripts

--- demo/BlazorMart/install-istio.sh
+++ demo/BlazorMart/install-istio.sh
@@ -1,1 +1,3 @@
 istioctl manifest apply --set profile=demo
+istioctl manifest apply --set values.kiali.enabled=true
+kubectl label namespace default istio-injection=enabled
--- demo/BlazorMart/run-k8s.sh
+++ demo/BlazorMart/run-k8s.sh
@@ -2,6 +2,12 @@
 kubectl delete -f resource-manifests
 
 kubectl apply -f resource-manifests/filter.yaml
-kubectl apply -f <(istioctl kube-inject -f resource-manifests/backend.yaml)
+kubectl apply -f resource-manifests/backend.yaml
 kubectl apply -f resource-manifests/frontend.yaml
 kubectl apply -f resource-manifests/gateway.yaml
+
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+echo ---
+echo Ingress URL: http://$MINIKUBE_IP:$INGRESS_PORT
+echo Ingress URL: https://$MINIKUBE_IP:$SECURE_INGRESS_PORT
